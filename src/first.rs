84ef0f23621c5192727b5d6855d86ad342f6eebe
Zhengping Jiang <z1.jiang@ssi.samsung.com>
2019-10-08 14:28:11 -0700

    better list

--- src/first.rs
+++ src/first.rs
@@ -1,4 +1,14 @@
-pub enum List {
-    Empty,
-    Elem(i32, Box<List>),
+pub struct List {
+    head: Link,
 }
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
